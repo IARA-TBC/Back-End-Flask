56e8827a24bf4531435d677490b664a6cd3ce48c
LuisEmbonStrizzi <luisembonstrizzi@gmail.com>
2022-09-15 08:20:06 -0300

    feat(LFS): Git LFS configurated

--- Server.py
+++ Server.py
@@ -4,6 +4,8 @@ from flask_cors import CORS
 import numpy as np
 import requests
 from useful_functions import model_predict_cnn, model_cnn
+from PIL import Image
+import io
 
 app = Flask(__name__)
 CORS(app)
@@ -21,15 +23,19 @@ def upload():
     req = requests.post('http://localhost:4000/images/sendFile', json=imagefile)
     print(req)
     print(req.content)
-    dictFromServer = req.json()
-    print(dictFromServer)
 
-    imagefile = request.form['file']
+    base64_data = req.content
+
+    image = Image.open(io.BytesIO(base64_data))
+    print(image)
+
+    '''imagefile = request.files['file']
     print(imagefile)
     image_path = "./images/" + imagefile.filename
     imagefile.save(image_path)
+    '''
 
-    preds = model_predict_cnn(image_path, model_cnn)
+    preds = model_predict_cnn(image, model_cnn)
 
     accuracy = float(np.max(preds, axis=1)[0])
     accuracy = str(round(accuracy * 100 , 2))
--- useful_functions.py
+++ useful_functions.py
@@ -1,29 +1,42 @@
 import numpy as np
 import io
+import keras.models
 import tensorflow as tf
 from skimage import transform
 from PIL import Image
 from keras.models import load_model
+from transformers import TFViTForImageClassification
 
 MODEL_PATH_CNN = './models/TBC_CNN_Fusion.h5'
 #MODEL_PATH_TRANSFORMERS = './models/WeightsInfer.h5'
 
 model_cnn = load_model(MODEL_PATH_CNN)
+
+with open("./models/config/ConfigTrain.json") as json_file:
+    json_config = json_file.read()
+transformer = keras.models.model_from_json(json_config)
+transformer.load_weights('./models/WeigthsTrain.h5')
 #model_Transformers = load_model(MODEL_PATH_TRANSFORMERS)
 
-def image_cnn(path):
-    np_image = Image.open(path)
+def image_cnn(image):
+    '''np_image = Image.open(path)
     np_image = np.array(np_image).astype('float32') / 255
     np_image = transform.resize(np_image, (256, 256, 3))
     np_image = np.expand_dims(np_image, axis=0)
     return np_image
+    '''
+    np_image = np.array(image).astype('float32') / 255
+    np_image = transform.resize(np_image, (256, 256, 3))
+    np_image = np.expand_dims(np_image, axis=0)
+    return np_image
 
-def image_transformers(path):
-    file = Image.open(io.BytesIO(path)).convert("RGB")
-    file = file.resize((224, 224))
-    file = np.array(file) / 255.0
-    file = np.moveaxis(file, -1, 0)
-    return file
+def image_transformers(image):
+    image = image.convert("RGB")
+    image = image.resize((224,224))
+    image = np.array(image) / 255.0
+    img = np.moveaxis(image, -1. 0)
+    img = np.expand_dims(img, axis=0)
+    return img
 
 def model_predict_cnn(img_path, model):
     preds = model.predict(image_cnn(img_path))
