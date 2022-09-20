d8fe7bef1e470f49b917a49e8295cd5511ad43d1
LuisEmbonStrizzi <luisembonstrizzi@gmail.com>
2022-09-20 14:58:05 -0300

    feat(average): Added average prediction

--- Server.py
+++ Server.py
@@ -25,21 +25,26 @@ def upload():
 
     image = Image.open(io.BytesIO(base64_data))
 
-    preds = model_predict_cnn(image)
-    predsTransformers = model_predict_transformers(image)
-    print('Transformers:', predsTransformers)
+    preds_cnn = model_predict_cnn(image)
+    preds_transformers = model_predict_transformers(image)
+    print('Transformers:', preds_transformers)
 
-    accuracy_cnn = float(np.max(preds, axis=1)[0])
-    accuracy_cnn = str(round(accuracy_cnn * 100 , 2))
+    accuracy_cnn_raw = float(np.max(preds_cnn, axis=1)[0])
+    accuracy_cnn = str(round(accuracy_cnn_raw * 100 , 2))
     accuracy_cnn = accuracy_cnn + ' %'
 
-    accuracy_transformers = float(np.max(preds, axis=1)[0])
-    accuracy_transformers = str(round(accuracy_transformers * 100 , 2))
+    accuracy_transformers_raw = float(np.max(preds_transformers, axis=1)[0])
+    accuracy_transformers = str(round(accuracy_transformers_raw * 100 , 2))
     accuracy_transformers = accuracy_transformers + ' %'
+
+    accuracy_average = (accuracy_cnn_raw + accuracy_transformers_raw)/2
+    accuracy_average = str(round(accuracy_average * 100 , 2))
+    accuracy_average = accuracy_average + ' %'
 
     return jsonify({
         'prediction_cnn': accuracy_cnn,
-        'prediction_transformers': accuracy_transformers
+        'prediction_transformers': accuracy_transformers,
+        'prediction_average': accuracy_average
     })
 
 
